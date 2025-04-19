dd2b55dca02647f8e71ca5586e579ac65bbf3592
STHEXA <ti.syu.s@gmail.com>
2025-04-19 13:56:32 +0900

    zodでバリデーション

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -1,9 +1,18 @@
 "use server";
+import { ContactSchema } from "@/validations/contact";
 import { redirect } from "next/navigation";
 
 export async function SubmitContactForm(formData: FormData) {
   const name = formData.get("name");
   const email = formData.get("email");
+
+  //バリデーション
+  const validationResult = ContactSchema.safeParse({ name, email });
+  if (!validationResult.success) {
+    const errors = validationResult.error.flatten();
+    console.log("サーバー側でエラー" + errors);
+    return {};
+  }
   console.log("送信されたデータ", { name, email });
   redirect("/contacts/complete");
 }
