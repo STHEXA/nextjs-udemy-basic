c04c7e45c56fd033ec0aab54c85a5fb7f58ea464
STHEXA <ti.syu.s@gmail.com>
2025-04-19 14:18:18 +0900

    useActionStateでサーバーのバリデーションをクライアントに表示

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -2,16 +2,34 @@
 import { ContactSchema } from "@/validations/contact";
 import { redirect } from "next/navigation";
 
-export async function SubmitContactForm(formData: FormData) {
+type ActionState = {
+  success: boolean;
+  errors: {
+    name?: string[];
+    email?: string[];
+  };
+  serverError?: string;
+};
+
+export async function SubmitContactForm(
+  prevState: ActionState,
+  formData: FormData
+): Promise<ActionState> {
   const name = formData.get("name");
   const email = formData.get("email");
 
   //バリデーション
   const validationResult = ContactSchema.safeParse({ name, email });
   if (!validationResult.success) {
-    const errors = validationResult.error.flatten();
+    const errors = validationResult.error.flatten().fieldErrors;
     console.log("サーバー側でエラー" + errors);
-    return {};
+    return {
+      success: false,
+      errors: {
+        name: errors.name || [],
+        email: errors.email || [],
+      },
+    };
   }
   console.log("送信されたデータ", { name, email });
   redirect("/contacts/complete");
