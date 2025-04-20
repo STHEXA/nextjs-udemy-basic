5a80290bc8cfce6ceec69315358f2f4c54efe1ce
STHEXA <ti.syu.s@gmail.com>
2025-04-20 15:40:51 +0900

    登録されたデータを表示

---
+++ src/app/contacts/list/page.tsx
@@ -0,0 +1,21 @@
+import { getContact, getContacts } from "@/lib/contact";
+import React from "react";
+
+export default async function ListPage() {
+  const contacts = await getContacts();
+  const first = await getContact("1");
+  return (
+    <div>
+      複数
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name} : {contact.email}
+          </li>
+        ))}
+      </ul>
+      1件
+      <div>{first ? first.name : "登録されていません"}</div>
+    </div>
+  );
+}
